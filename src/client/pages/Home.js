e1ad685a6ccff3b0537587942742b3b2409817ea
Arrettino <batukearrettino@gmail.com>
2020-10-26 13:34:30 -0300

    add ssr configuration

---
+++ src/client/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/style/app.css';
+import react from '../assets/static/react.png';
+
+const Home = () => (
+  <div>
+    <p>
+      mi mama me mima
+    </p>
+    <img src={react} alt='react' />
+  </div>
+);
+
+export default Home;
